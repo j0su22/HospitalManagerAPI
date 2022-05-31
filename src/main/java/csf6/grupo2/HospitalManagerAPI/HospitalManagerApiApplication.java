712c6c0bf2319a3fcd5e9ef955662aee74a23580
Josue Cortez <josue.cortez222@gmail.com>
2022-05-31 15:52:36 -0600

    ADD: Proyecto base con Spring Boot, Dependencias: -Spring WEB -Spring Data JPA -OAuth2 Client -MS SQL Server Driver -Lombok

---
+++ src/main/java/csf6/grupo2/HospitalManagerAPI/HospitalManagerApiApplication.java
@@ -0,0 +1,13 @@
+package csf6.grupo2.HospitalManagerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/csf6/grupo2/HospitalManagerAPI/HospitalManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package csf6.grupo2.HospitalManagerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
