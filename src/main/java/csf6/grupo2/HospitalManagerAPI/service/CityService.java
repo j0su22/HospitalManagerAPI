0bd7fcacae0cc67aec4b1cc90ef175bcba85cd58
Josue Cortez <josue.cortez222@gmail.com>
2022-06-01 14:42:50 -0600

    ADD: lo necesario para CITY

--- src/main/java/csf6/grupo2/HospitalManagerAPI/service/CityService.java
+++ src/main/java/csf6/grupo2/HospitalManagerAPI/service/CityService.java
@@ -3,9 +3,12 @@ package csf6.grupo2.HospitalManagerAPI.service;
 import csf6.grupo2.HospitalManagerAPI.model.City;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CityService {
     City saveCity(City city);
 
     List<City> getAllCities();
+
+    Optional<City> findCities(Integer id);
 }
